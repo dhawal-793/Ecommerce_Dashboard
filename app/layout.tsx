905b2dd8b30dbb86ca18aabfaa97b24ecd262085
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-05 22:28:58 +0530

    Added ClerkProvider in rootLayout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+
+import { ClerkProvider } from '@clerk/nextjs'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -13,9 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-  
+    <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
+    </ClerkProvider>
   )
 }
