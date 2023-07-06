ca28ce56f26472ac90c26f725b6fb338c6f7bc78
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-06 21:23:30 +0530

    Added Modalprovider in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import './globals.css'
 import { Inter } from 'next/font/google'
-
 import { ClerkProvider } from '@clerk/nextjs'
+
+import './globals.css'
+import { ModalProvider } from '@/providers/modal-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -17,7 +18,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
