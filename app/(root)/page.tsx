b2dc7c529fe79fbcb0ddad9346d900528b1feb55
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-06 21:24:21 +0530

    Updated home page to show a modal.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,26 @@
+'use client'
+
 import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 export default function Home() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [onOpen, isOpen])
+
   return (
-    <div className="flex justify-between items-center p-4">
-      <p>Home</p>
-      <UserButton afterSignOutUrl="/"/>
-    </div>
-    
+    <>
+      <div className="flex items-center justify-end p-4">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </>
+
   )
 }
