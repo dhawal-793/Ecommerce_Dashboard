49ef35a624db29af827991939cf4de7655ca6711
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-07 21:22:36 +0530

    Udated storeModal to use zod and use-for-hook inside store-modal

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -2,13 +2,36 @@
 
 import { useStoreModal } from "@/hooks/use-store-modal"
 import Modal from "@/components/ui/modal"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+
+const formSchema = z.object({
+    name: z.string().min(1)
+})
+
 
 export const StoreModal = () => {
     const storeModal = useStoreModal()
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: ""
+        }
+    })
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        // Todo: Create Store
+        console.log(values);
+
+    }
+
     return (
         <Modal
             title="Create Store"
-            description="Create a new store"
+            description="Create a brand new store"
             isOpen={storeModal.isOpen}
             onClose={storeModal.onClose}
         >
