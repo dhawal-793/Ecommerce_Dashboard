0923d2ce841b479c0c5362a10915bd02f4a2af95
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-08 13:26:08 +0530

    Added actual api call inside onSubmit  with axios.

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -3,6 +3,7 @@
 import { useStoreModal } from "@/hooks/use-store-modal"
 import Modal from "@/components/ui/modal"
 import * as z from "zod"
+import axios from "axios"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
@@ -26,8 +27,18 @@ export const StoreModal = () => {
     })
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        // Todo: Create Store
-        console.log(values);
+        try {
+            setLoading(true)
+            const response = await axios.post("/api/stores", values)
+            console.log("response", response.data);
+
+        } catch (error) {
+            console.log(error);
+
+        }
+        finally {
+            setLoading(false)
+        }
 
     }
 
