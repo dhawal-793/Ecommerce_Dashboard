7b997052e18431a61c08cc5978a8ccf172b5b8e6
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-08 13:38:16 +0530

    Added toast-provider inside layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 
 import './globals.css'
 import { ModalProvider } from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToastProvider />
           <ModalProvider />
           {children}
         </body>
