dba3e1d8aecfe8106ca1243591b88c6a1cc9ee9a
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-09 12:27:41 +0530

    Updated MAinNav to display links for routes.

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+'use client'
 
-const MainNav = () => {
+import { cn } from "@/lib/utils"
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation"
+
+
+const MainNav = ({ className, ...props }:
+    React.HTMLAttributes<HTMLElement>) => {
+    const pathName = usePathname();
+    const params = useParams()
+
+    const routes = [
+        {
+            href: `/${params.storeId}/settings`,
+            label: "settings",
+            active: pathName === `/${params.storeId}/settings`
+        },
+    ]
+
     return (
-        <div>MainNav</div>
+        <nav className={cn('flex items-center space-x-4 lg:space-x-6', className)}>
+
+            {
+                routes.map(({ active, label, href }) => (
+                    <Link key={href} href={href}
+                        className={cn('text-sm font-medium transition-colors hover:text-primary', active ? 'text-black dark:text-white' : 'text-muted-foreground')}
+                    >
+                        {label}
+                    </Link>
+                ))
+            }
+        </nav>
     )
 }
 
