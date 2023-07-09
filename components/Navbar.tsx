40074a5f4ef1ee5b74977c75056553a64bcdfe95
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-09 12:41:22 +0530

    Added: fetch userId and stores & pass to StoreSwitcher

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,24 @@
-import { UserButton } from '@clerk/nextjs'
+import { UserButton, auth } from '@clerk/nextjs'
 import MainNav from '@/components/MainNav'
 import StoreSwitcher from './Store-Switcher'
+import { redirect } from 'next/navigation'
+import prismaDb from '@/lib/prismaDb'
 
 
-const Navbar = ({ }) => {
+const Navbar = async ({ }) => {
+    const { userId } = auth()
+    if (!userId) redirect('/sign-in')
 
+    const stores = await prismaDb.store.findMany({
+        where: {
+            userId
+        }
+    })
 
     return <div className='border-b '>
         <div className="flex items-center h-16 px-4">
-            <StoreSwitcher />
-            <MainNav className='mx-6' />
+            <StoreSwitcher items={stores} />
+            <MainNav className='mx-6'/>
             <div className='flex items-center ml-auto space-x-4'>
                 <UserButton afterSignOutUrl='/' />
             </div>
