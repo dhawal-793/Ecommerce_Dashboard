09c625d856cb628d30bfb4893d0f042dd18c6b13
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-09 19:27:16 +0530

    Added Link for overview page fro current store

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -12,6 +12,11 @@ const MainNav = ({ className, ...props }:
 
     const routes = [
         {
+            href: `/${params.storeId}`,
+            label: "Overview",
+            active: pathName === `/${params.storeId}`
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: "settings",
             active: pathName === `/${params.storeId}/settings`
