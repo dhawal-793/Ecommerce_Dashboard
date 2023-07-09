d5d320b6f991571e78afd424376ee4c05f378818
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-09 18:57:34 +0530

    Created API route for deleting a store.

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -27,3 +27,25 @@ export async function PATCH(req: Request, { params }: { params: { storeId: strin
 
     }
 }
+
+export async function DELETE(req: Request, { params }: { params: { storeId: string } }) {
+    try {
+        const { userId } = auth()
+        if (!userId) return new NextResponse("Unauthorized", { status: 401 })
+
+        const { storeId } = params
+        if (!storeId) return new NextResponse("StoreId is required", { status: 400 })
+
+        const store = await prismaDb.store.deleteMany({
+            where: { id: storeId, userId }
+        })
+
+        return NextResponse.json(store)
+
+
+    } catch (error) {
+        console.error(`[STORE_DELETE] =>`, error)
+        return new NextResponse("Internal Server Error", { status: 500 })
+
+    }
+}
