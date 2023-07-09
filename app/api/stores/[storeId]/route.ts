ca4d10349e8a4baf66e3996b68ee632f7be670ec
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-09 18:52:35 +0530

    Added API route to update the store

---
+++ app/api/stores/[storeId]/route.ts
@@ -0,0 +1,29 @@
+import prismaDb from "@/lib/prismaDb"
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+export async function PATCH(req: Request, { params }: { params: { storeId: string } }) {
+    try {
+        const { userId } = auth()
+        if (!userId) return new NextResponse("Unauthorized", { status: 401 })
+
+        const { name } = await req.json()
+        if (!name) return new NextResponse("Name is required", { status: 400 })
+
+        const { storeId } = params
+        if (!storeId) return new NextResponse("StoreId is required", { status: 400 })
+
+        const store = await prismaDb.store.updateMany({
+            where: { id: storeId, userId },
+            data: { name }
+        })
+
+        return NextResponse.json(store)
+
+
+    } catch (error) {
+        console.error(`[STORE_PATCH] =>`, error)
+        return new NextResponse("Internal Server Error", { status: 500 })
+
+    }
+}
