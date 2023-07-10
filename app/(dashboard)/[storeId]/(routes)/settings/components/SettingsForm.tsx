36c28203e24278a66dc6e1f067bd7f4fcda2977d
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-10 15:33:29 +0530

    Added use-origin and api-alert components.

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -1,26 +1,27 @@
 'use client'
 
-import AlertModal from '@/components/modals/alert-modal'
-import { Button } from '@/components/ui/button'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
-import Heading from '@/components/ui/heading'
-import { Input } from '@/components/ui/input'
-import { Separator } from '@/components/ui/separator'
-import { zodResolver } from '@hookform/resolvers/zod'
 import { Store } from '@prisma/client'
-import axios from 'axios'
-import { Trash } from 'lucide-react'
-import { useParams, useRouter } from 'next/navigation'
 import { FC, useState } from 'react'
+import { useParams, useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Trash } from 'lucide-react'
 import { toast } from 'react-hot-toast'
 import * as z from 'zod'
+import axios from 'axios'
+
+import AlertModal from '@/components/modals/alert-modal'
+import ApiAlert from '@/components/ui/api-alert'
+import Heading from '@/components/ui/heading'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Separator } from '@/components/ui/separator'
+import { UseOrigin } from '@/hooks/use-origin'
 
 interface SettingsFormProps {
     initialData: Store
 }
-
-
 
 const formSchema = z.object({
     name: z.string().min(1)
@@ -31,10 +32,13 @@ type SettingsformVaues = z.infer<typeof formSchema>
 
 const SettingsForm: FC<SettingsFormProps> = ({ initialData }) => {
 
-    const [open, setOpen] = useState<boolean>(false)
-    const [loading, setLoading] = useState<boolean>(false)
     const params = useParams()
     const router = useRouter()
+    const origin = UseOrigin()
+
+    const [open, setOpen] = useState<boolean>(false)
+    const [loading, setLoading] = useState<boolean>(false)
+
     const form = useForm<SettingsformVaues>({
         resolver: zodResolver(formSchema),
         defaultValues: initialData
@@ -107,6 +111,8 @@ const SettingsForm: FC<SettingsFormProps> = ({ initialData }) => {
                     >Save Changes</Button>
                 </form>
             </Form>
+            <Separator />
+            <ApiAlert title='NEXT_PUBLIC_API_URL' description={`${origin}/api/${params.storeId}`} variant='admin' />
         </>
 
     )
