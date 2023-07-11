c02f447ed701a245d3d82039e6baef27b279d434
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-11 23:53:03 +0530

    Created BillboardForm for creating/updating billboards.

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillboardForm.tsx
@@ -0,0 +1,158 @@
+'use client'
+
+import { FC, useState } from 'react'
+import { useParams, useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Billboard } from '@prisma/client'
+import { Trash } from 'lucide-react'
+import { toast } from 'react-hot-toast'
+import * as z from 'zod'
+import axios from 'axios'
+
+import AlertModal from '@/components/modals/alert-modal'
+import Heading from '@/components/ui/heading'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Separator } from '@/components/ui/separator'
+import ImageUpload from '@/components/ui/image-upload'
+
+const formSchema = z.object({
+    label: z.string().min(1),
+    imageUrl: z.string().min(1)
+})
+
+type BillboardformVaues = z.infer<typeof formSchema>
+
+interface BillboardFormProps {
+    initialData: Billboard | null
+}
+
+const BillboardForm: FC<BillboardFormProps> = ({ initialData }) => {
+
+    const params = useParams()
+    const router = useRouter()
+
+    const [open, setOpen] = useState<boolean>(false)
+    const [loading, setLoading] = useState<boolean>(false)
+
+    const title = initialData ? "Edit Billboard" : "Create Billboard"
+    const description = initialData ? "Edit a billboard" : "Add a new billboard"
+    const toastMessage = initialData ? "Billboard updated" : "Billboard created"
+    const action = initialData ? "Save Changes" : "Create"
+
+    const form = useForm<BillboardformVaues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData || {
+            label: '',
+            imageUrl: '',
+        }
+    })
+
+    const onSubmit = async (data: BillboardformVaues) => {
+        try {
+            setLoading(true)
+            if (initialData) {
+                await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data)
+            }
+            else {
+                await axios.post(`/api/${params.storeId}/billboards`, data)
+            }
+            router.refresh()
+            toast.success(toastMessage)
+
+        } catch (error) {
+            toast.error("Something went wrong")
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+
+    const onDelete = async () => {
+        try {
+            setLoading(true)
+            await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`)
+            router.refresh()
+            toast.success("Billboard Deleted.")
+
+        } catch (error) {
+            toast.error("Make sure to remove all categories using this billboard first.")
+        }
+        finally {
+            setLoading(false)
+            setOpen(false)
+        }
+    }
+
+    return (
+        <>
+            <AlertModal isOpen={open} onClose={() => setOpen(false)} loading={loading} onConfirm={onDelete} />
+            <div className='flex items-center justify-between' >
+                <Heading title={title} description={description} />
+                {initialData && (
+                    <Button
+                        variant="destructive"
+                        disabled={loading}
+                        size="sm"
+                        onClick={() => setOpen(true)}
+                    >
+                        <Trash className='w-4 h-4' />
+                    </Button>
+                )}
+            </div>
+            <Separator />
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8 wfull'>
+                    <FormField
+                        control={form.control}
+                        name="imageUrl"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Background Image</FormLabel>
+                                <FormControl>
+                                    <ImageUpload
+                                        value={field.value ? [field.value] : []}
+                                        disabled={loading}
+                                        onChange={(url) => field.onChange(url)}
+                                        onRemove={() => field.onChange("")}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <div className="grid grid-cols-3 gap-8">
+                        <FormField
+                            control={form.control}
+                            name="label"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Label</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            disabled={loading}
+                                            placeholder='Billboard Label'
+                                            {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <Button
+                        disabled={loading}
+                        type="submit"
+                        className='ml-auto'
+                    >
+                        {action}
+                    </Button>
+                </form>
+            </Form>
+            <Separator />
+        </>
+
+    )
+}
+export default BillboardForm
