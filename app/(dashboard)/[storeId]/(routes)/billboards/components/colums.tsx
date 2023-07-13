1b16f65223a60984280a02773cb9ab85b60de4e1
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-13 10:46:17 +0530

    Updated colums to add actions with CellActions

--- app/(dashboard)/[storeId]/(routes)/billboards/components/colums.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/colums.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+import { CellAction } from "./cell-action"
 
 
 export type BillboardColumn = {
@@ -18,5 +19,9 @@ export const columns: ColumnDef<BillboardColumn>[] = [
     {
         accessorKey: "createdAt",
         header: "Date",
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => <CellAction data={row.original} />
     }
 ]
