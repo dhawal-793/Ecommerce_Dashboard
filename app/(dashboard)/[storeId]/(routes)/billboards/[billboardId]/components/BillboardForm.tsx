3cdcc0237b47f169daba68e899df1ee5ce7781bf
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-13 09:32:01 +0530

    Updated route after creating/updating a billboard.

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillboardForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillboardForm.tsx
@@ -60,6 +60,7 @@ const BillboardForm: FC<BillboardFormProps> = ({ initialData }) => {
                 await axios.post(`/api/${params.storeId}/billboards`, data)
             }
             router.refresh()
+            router.push(`/${params.storeId}/billboards`)
             toast.success(toastMessage)
 
         } catch (error) {
