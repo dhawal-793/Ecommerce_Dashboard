eafa1ec9c5ddbbb238fad704992c0d2b375fdb54
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-13 09:30:38 +0530

    Fetched all billboards, passed to BillboardClient.

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,10 +1,35 @@
+
+
+
+import prismaDb from "@/lib/prismaDb"
 import BillboardClient from "./components/BillboardClient"
 
-const Billboards = () => {
+
+
+
+import { FC } from 'react'
+
+interface BillboardsProps {
+    params: {
+        storeId: string
+    }
+}
+
+const Billboards: FC<BillboardsProps> = async ({ params }) => {
+    const billboards = await prismaDb.billboard.findMany({
+        where: {
+            storeId: params.storeId
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
+    
     return (
         <div className="flex flex-col">
             <div className="flex-1 p-8 pt-6 space-y-4">
-                <BillboardClient />
+                <BillboardClient data={billboards} />
             </div>
         </div>
     )
