63a0184b0fc1dc64fb1935be19ca94d3e6b70209
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-19 13:13:19 +0530

    Added PATCH route for /categories/[categoryId]

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -1,6 +1,45 @@
+import { auth } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
 
 import prismaDb from "@/lib/prismaDb"
+
+export async function PATCH(req: Request, { params }: { params: { storeId: string, categoryId: string } }) {
+    try {
+
+        const { userId } = auth()
+        if (!userId) return new NextResponse("Unauthenticated", { status: 401 })
+
+        const { name, billboardId } = await req.json()
+        if (!name) return new NextResponse("Name is required", { status: 400 })
+        if (!billboardId) return new NextResponse("BillboardId is required", { status: 400 })
+
+        const { storeId, categoryId } = params
+        if (!storeId) return new NextResponse("StoreId is required", { status: 400 })
+        if (!categoryId) return new NextResponse("CategoryId is required", { status: 400 })
+
+        const storeByUserId = await prismaDb.store.findFirst({
+            where: {
+                id: params.storeId,
+                userId
+            }
+        })
+        if (!storeByUserId) {
+            return new NextResponse("Unauthorized", { status: 403 })
+        }
+
+        const category = await prismaDb.category.updateMany({
+            where: {
+                id: categoryId,
+            },
+            data: { name, billboardId }
+        })
+        return NextResponse.json(category)
+
+    } catch (error) {
+        console.error(`[CATEGORY_PATCH] =>`, error)
+        return new NextResponse("Internal Server Error", { status: 500 })
+    }
+}
 
 export async function GET(req: Request, { params }: { params: { categoryId: string } }) {
     try {
