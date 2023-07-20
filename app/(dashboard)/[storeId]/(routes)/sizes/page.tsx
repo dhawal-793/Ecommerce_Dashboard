0c9ce1b075249acbfe19c0ef9bf96e9c1f900285
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-20 15:13:23 +0530

    Created sizes route with SizeClient.

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -0,0 +1,44 @@
+
+import { format } from 'date-fns'
+
+import prismaDb from "@/lib/prismaDb"
+import SizeClient from "./components/SizeClient"
+
+
+import { FC } from 'react'
+import { SizeColumn } from "./components/colums"
+
+interface SizesProps {
+    params: {
+        storeId: string
+    }
+}
+
+const Sizes: FC<SizesProps> = async ({ params }) => {
+    const sizes = await prismaDb.size.findMany({
+        where: {
+            storeId: params.storeId
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
+    const formattedSizes: SizeColumn[] = sizes.map(size => ({
+
+        id: size.id,
+        name: size.name,
+        value:size.name,
+        createdAt: format(size.createdAt, "MMMM, do, yyyy")
+    }))
+
+    return (
+        <div className="flex flex-col">
+            <div className="flex-1 p-8 pt-6 space-y-4">
+                <SizeClient data={formattedSizes} />
+            </div>
+        </div>
+    )
+}
+
+export default Sizes
