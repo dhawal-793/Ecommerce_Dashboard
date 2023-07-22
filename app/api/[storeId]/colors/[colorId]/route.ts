485acb479de01ef8f380464b80eeea7d1a2cb72a
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-22 12:45:21 +0530

    Created DELETE API route for /colors/[colorId].

--- app/api/[storeId]/colors/[colorId]/route.ts
+++ app/api/[storeId]/colors/[colorId]/route.ts
@@ -42,6 +42,38 @@ export async function PATCH(req: Request, { params }: { params: { storeId: strin
     }
 }
 
+export async function DELETE(req: Request, { params }: { params: { storeId: string, colorId: string } }) {
+    try {
+
+        const { userId } = auth()
+        if (!userId) return new NextResponse("Unauthenticated", { status: 401 })
+
+        const { storeId, colorId } = params
+        if (!storeId) return new NextResponse("StoreId is required", { status: 400 })
+        if (!colorId) return new NextResponse("ColorId is required", { status: 400 })
+
+        const storeByUserId = await prismaDb.store.findFirst({
+            where: {
+                id: params.storeId,
+                userId
+            }
+        })
+        if (!storeByUserId) {
+            return new NextResponse("Unauthorized", { status: 403 })
+        }
+
+        const color = await prismaDb.color.deleteMany({
+            where: { id: colorId }
+        })
+
+        return NextResponse.json(color)
+
+    } catch (error) {
+        console.error(`[COLOR_DELETE] =>`, error)
+        return new NextResponse("Internal Server Error", { status: 500 })
+    }
+}
+
 export async function GET(req: Request, { params }: { params: { colorId: string } }) {
     try {
 
