8075cf6301d22ad74790180ab69c1d15fac26ce4
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-22 13:27:39 +0530

    Created method to format currency.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const formatter = new Intl.NumberFormat("en-US", {
+  style: 'currency',
+  currency: 'INR'
+})
