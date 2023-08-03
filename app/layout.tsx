cd592e4e211a0532d06c3089377645f879b7d63f
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-03 09:36:31 +0530

    Added ThemeProvider in root layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
+import { ThemeProvider } from '@/providers/theme-provider'
 import { ModalProvider } from '@/providers/modal-provider'
 import ToastProvider from '@/providers/toast-provider'
 
@@ -22,9 +23,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ToastProvider />
-          <ModalProvider />
-          {children}
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem >
+            <ToastProvider />
+            <ModalProvider />
+              {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
