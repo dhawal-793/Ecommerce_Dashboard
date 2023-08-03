7f42746d8372ac5657f67e83e59c9d5bc11ea200
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-03 09:26:08 +0530

    Created OverviewChart component to display sales.

---
+++ components/OverviewChart.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { FC } from 'react'
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+
+interface OverviewChartProps {
+    data: any[]
+}
+
+const OverviewChart: FC<OverviewChartProps> = ({ data }) => {
+    return (
+        <ResponsiveContainer width="100%" height={350}>
+            <BarChart data={data}>
+                <XAxis
+                    dataKey="name"
+                    stroke="#888888"
+                    fontSize={12}
+                    tickLine={false}
+                    axisLine={false}
+                />
+                <YAxis
+                    stroke="#888888"
+                    fontSize={12}
+                    tickLine={false}
+                    axisLine={false}
+                    tickFormatter={(value) => `₹${value}`}
+                />
+                <Bar dataKey="total" fill="#3498ab" radius={[4, 4, 0, 0]} />
+            </BarChart>
+        </ResponsiveContainer>
+    )
+}
+
+export default OverviewChart
